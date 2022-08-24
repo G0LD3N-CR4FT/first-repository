24dfdb77b9dd3000dd1728d3904f4614bef8f6ac
G0LD3N-CR4FT <eduardomaster.tellis@gmail.com>
2022-08-24 08:56:11 -0300

    Atualização 24/08/2022 08:56

---
+++ js/aula24.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+$('p').hide()
+
+// Monitorar o click no botão "read"
+$('#read').click(function(e){
+    e.preventDefault()
+    
+    $('p').toggle(1500, function(){
+        // Criar uma váriavel para receber o status
+        let status = $('#read').attr('data-status')
+        // Limpando label do btn
+        $('#read').empty()
+        // Testando o status 
+        if(status == 1){
+           $('#read').append('Esconder resumo')
+           $('#read').attr('data-status','2')
+        }
+        else {
+           $('#read').append('Ver resumo')
+           $('#read').attr('data-status','1')
+        }
+    })
+
+  })
+})
